9e5da51006f7e8416312cb7f0e953942085392de
Luis Rodrigues <luis.ms.rodrigues@hotmail.com>
2021-03-12 12:00:28 +0000

    added http-client

--- webpack/webpack.config.common.js
+++ webpack/webpack.config.common.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   entry: ['./src/http-client/js/app.js', './src/http-client/style/main.scss'],
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '..', 'dist'),
     filename: 'app.bundle.js',
   },
   module: {
