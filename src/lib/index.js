ea9d7d6705c26e1349a30acdc4056ab01a8f8a70
Luis Rodrigues <luis.ms.rodrigues@hotmail.com>
2021-03-18 19:35:03 +0000

    removed alert

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,10 +5,5 @@ const DEBUG = require('debug')('LIB');
 
 (async function () {
     DEBUG("STARTING");
-
-    let result = await UTILS.Alert.Error({title: "TEST TITLE", message: "TEST MESSAGE"}); 
-    DEBUG(result);
-    DEBUG("DONE");
-
     await require('./services')(CONFIGURATION, GLOBAL_STATE, UTILS);
 })();
